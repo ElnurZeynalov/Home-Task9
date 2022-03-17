36a2367f4a7f5daacfae590a1ab29b194f5698d9
ElnurZeynalov <tu74449ce@code.edu.az>
2022-03-17 11:32:42 +0400

    Task Complete

---
+++ ConsoleApp1/ConsoleApp1/Group.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Group
+    {
+        private string _no;
+        private int _studentlimit;
+
+        public string No
+        {
+            get
+            {
+                return _no;
+            }
+            set
+            {
+                if (Checker(value))
+                    _no = value;
+            }
+        }
+        public int Student
+        {
+            get
+            {
+                return _studentlimit;
+            }
+            set
+            {
+                if(value > 0)
+                _studentlimit = value;
+            }
+        }
+        static bool Checker(string value)
+        {
+            if (!string.IsNullOrEmpty(value) && value.Length == 5)
+            {
+                if (char.IsUpper(value[0]) && char.IsUpper(value[1]) && char.IsDigit(value[2]) && char.IsDigit(value[3]) && char.IsDigit(value[4]))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Group group = new Group();
+            group.No = Console.ReadLine();
+            group.Student = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(group.No);
+            Console.WriteLine(group.Student);
+        }
+    }
+}
